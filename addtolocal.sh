b99eff7908ea5d2f58e343a3885aa6b00391586e
Matthew Strapp <matt@mattstrapp.net>
2021-09-01 07:20:00 -0500

    Replace link with copy

--- addtolocal.sh
+++ addtolocal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ln -sf "$(pwd)/sysd2v.sh" ~/.local/bin/sysd2v && echo "Installed successfully to ~/.local/bin" || echo "Installation failed."
+cp --parents "$(pwd)/sysd2v.sh" ~/.local/bin/sysd2v && echo "Installed successfully to ~/.local/bin" || echo "Installation failed."
