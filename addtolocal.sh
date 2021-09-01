401c052e00d2e36e68d4181031c09dbab1f390d5
Matt Strapp <matt@mattstrapp.net>
2021-09-01 07:26:59 -0500

    Fix install script because I'm dumb

--- addtolocal.sh
+++ addtolocal.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cp --parents "$(pwd)/sysd2v.sh" ~/.local/bin/sysd2v && echo "Installed successfully to ~/.local/bin" || echo "Installation failed."
+cp "$(pwd)/sysd2v.sh" ~/.local/bin/sysd2v && echo "Installed successfully to ~/.local/bin" || echo "Installation failed."
